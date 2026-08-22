ff244d842d4f702fa2839efc00667f7f3932b7c6
15x <1103409364@qq.com>
2026-08-22 13:15:15 +0800

    refactor: rename BpmnApp view to BpmnDesigner

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import BpmnApp from '../views/BpmnApp.vue'
+import BpmnDesigner from '../views/BpmnDesigner.vue'
 import LogicTree from '../views/LogicTree.vue'
 
 const routes = [
   { path: '/', redirect: '/bpmn' },
   {
     path: '/bpmn',
-    name: 'BpmnApp',
-    component: BpmnApp
+    name: 'BpmnDesigner',
+    component: BpmnDesigner
   },
   {
     path: '/logic-tree',
